d6cbd24e20ebddd3fb20881fa7cc438ded6639fc
gilbertexbom <gilberto.braga0021@gmail.com>
2023-12-01 14:08:09 -0300

    - Commit inicial do tio.

---
+++ conversor.py
@@ -0,0 +1,10 @@
+# Sistema de conversão de temperatura ºC <-> ºF
+
+# Método de conversão ºC para ºF
+def cel_fah(cel):
+    return 9 * cel / 5 + 32
+
+# teste
+# print(f'{0}ºC = {cel_fah(0)}ºF')
+# print(f'{-40}ºC = {cel_fah(-40)}ºF')
+# print(f'{38}ºC = {cel_fah(38)}ºF')

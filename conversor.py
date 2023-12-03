f7278c648063cc5e3f2126562be2886a476a0c30
Davi <dumwebtecnologia@gmail.com>
2023-12-03 16:29:41 -0300

    commit do Edson

--- conversor.py
+++ conversor.py
@@ -8,3 +8,10 @@ def cel_fah(cel):
 # print(f'{0}ºC = {cel_fah(0)}ºF')
 # print(f'{-40}ºC = {cel_fah(-40)}ºF')
 # print(f'{38}ºC = {cel_fah(38)}ºF')
+
+def fah_cel(fah):
+    return (fah - 32) * 5 / 9
+#test
+# print(f'{fah_cel(32)}ºC = {32}ºF')
+# print(f'{fah_cel(-40)}ºC = {-40}ºF')
+# print(f'{fah_cel(100.4)}ºC = {100.4}ºF')
